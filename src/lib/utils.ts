0683f5041dd8465c8ba73fc5324c8b84cf8f462c
toykam <kabirtoyyib19@gmail.com>
2025-05-09 07:39:14 +0100

    updated middleware to handle different webhook secret

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,4 +8,6 @@ export function cn(...inputs: ClassValue[]) {
 export const TOKEN_FACTORY_CONTRACT = process.env.TOKEN_FACTORY_CONTRACT!;
 export const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY!;
 export const DEX_CONTRACT = process.env.DEX_CONTRACT!;
+export const NEYNAR_MENTION_WEBHOOK_SECRET = process.env.NEYNAR_MENTION_WEBHOOK_SECRET!;
+export const NEYNAR_ENGAGEMENT_WEBHOOK_SECRET = process.env.NEYNAR_ENGAGEMENT_WEBHOOK_SECRET!;
 export const ENGAGEMENT_WEBHOOK_ID = "01JTRXPBEQED2GN3XWZ2SEDHYD";
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import CryptoJS from 'crypto-js';
+import { NEYNAR_ENGAGEMENT_WEBHOOK_SECRET, NEYNAR_MENTION_WEBHOOK_SECRET } from './lib/utils';
 
 export const middleware = async (request: NextRequest) => {
     console.log("PATH ::: ", request.nextUrl.pathname);
@@ -14,12 +15,18 @@ export const middleware = async (request: NextRequest) => {
       return NextResponse.json({ error: 'Neynar signature missing' }, { status: 400 });
     }
 
-    const webhookSecret = process.env.NEYNAR_WEBHOOK_SECRET;
+    let webhookSecret;
+    if (request.nextUrl.pathname.startsWith('/api/warpcast/mention')) {
+      webhookSecret = NEYNAR_MENTION_WEBHOOK_SECRET;
+    }
+    if (request.nextUrl.pathname.startsWith('/api/warpcast/engagement')) {
+      webhookSecret = NEYNAR_ENGAGEMENT_WEBHOOK_SECRET;
+    }
+
     if (!webhookSecret) {
       console.error('NEYNAR_WEBHOOK_SECRET not configured');
       return NextResponse.json({ error: 'Server configuration error' }, { status: 500 });
     }
-
     // Generate HMAC-SHA512 using CryptoJS
     const generatedSignature = CryptoJS.HmacSHA512(body, webhookSecret).toString(CryptoJS.enc.Hex);
 
