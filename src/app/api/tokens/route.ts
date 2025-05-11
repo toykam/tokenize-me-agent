b3f3e69f0df11bb98bdd6b49aec7eddbd8ba24b8
toykam <kabirtoyyib19@gmail.com>
2025-05-11 03:01:51 +0100

    added profile image support

--- src/app/api/tokens/route.ts
+++ src/app/api/tokens/route.ts
@@ -1,19 +1,35 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
-
 export async function GET() {
     try {
-        
         const tokens = await prisma.token.findMany({
             select: {
-                user: true, address: true, name: true, decimals: true, symbol: true, createdAt: true,
-                _count: {select: {Engagement: true, transactions: true}}
+                user: true,
+                address: true,
+                name: true,
+                decimals: true,
+                symbol: true,
+                createdAt: true,
+                _count: {
+                    select: {
+                        Engagement: true,
+                        transactions: true
+                    }
+                }
             },
-            orderBy: {
-                Engagement: {_count: 'desc'},
-                transactions: {_count: 'desc'},
-            }
+            orderBy: [
+                {
+                    Engagement: {
+                        _count: 'desc'
+                    }
+                },
+                {
+                    transactions: {
+                        _count: 'desc'
+                    }
+                }
+            ]
         });
 
         return NextResponse.json(tokens);
