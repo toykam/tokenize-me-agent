ea0036ab7836f6e747810e4c7380329cc1469971
toykam <kabirtoyyib19@gmail.com>
2025-05-14 12:11:30 +0100

    added some design changes

--- src/components/account/HoldingIcon.tsx
+++ src/components/account/HoldingIcon.tsx
@@ -1,7 +1,7 @@
 import { Token } from '@/lib/types'
 import Link from 'next/link'
 import React from 'react'
-import { motion, scale } from "framer-motion"
+import { motion } from "framer-motion"
 
 export default function HoldingItemComponent({token} : {token: Token}) {
   return (
